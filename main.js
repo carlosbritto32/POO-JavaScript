a63be38fc235361f277e0079c98b746e88c5daed
Carlos Britto <carlosbritto32@gmail.com>
2021-09-22 09:36:49 -0500

    creamos super clase, se la heredamos a la clase

--- main.js
+++ main.js
@@ -1,24 +1,65 @@
+function videoPlay(id) {
+  const urlSecreta = "https://platzi.com" + id;
+
+  console.log("Se esta reproduiendo desde la url" + urlSecreta);
+}
+
+function videoStop(id) {
+  const urlSecreta = "https://platzi.com" + id;
+
+  console.log("Pausamos la url" + urlSecreta);
+}
+
+// function videoPlay(){}
+
+class PlatziClass {
+  constructor({ name, videoID }) {
+    this.name = name;
+    this.videoID = videoID;
+  }
+
+  reproducir() {
+    videoPlay(this.videoID);
+  }
+
+  pausar() {
+    videoStop(this.videoID);
+  }
+}
+
 class Course {
-  constructor({ name, classes }) {
+  constructor({ name, classes = [], isFree = false, lang = "spanish" }) {
     this.name = name;
     this.classes = classes;
+    this.isFree = isFree;
+    this.lang = lang;
   }
 }
 
 const Html = new Course({
   name: "html",
+  isFree: true,
+  lang: "spanish",
 });
 const Css = new Course({
   name: "css",
+  isFree: true,
+  lang: "spanish",
 });
 const Js = new Course({
   name: "JS",
+  isFree: true,
+  lang: "spanish",
 });
 const Js_POO = new Course({
   name: "JS POO",
+  isFree: false,
+  lang: "english",
 });
 const React = new Course({
   name: "React",
+  isFree: false,
+  lang: "english",
 });
 
 class learningPaths {
@@ -40,6 +81,8 @@ const escuelaVdjgs = new learningPaths({
   name: "Escuela de videoJuegos",
   courses: [Html, Css, Js],
 });
+
+// clase de herencia 14
 
 class Student {
   constructor({
@@ -66,7 +109,49 @@ class Student {
   }
 }
 
-const juan2 = new Student({
+class freeStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    if (newCourse.isFree) {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn(
+        "Lo Sentimos, " + this.name + "Solo puedes tomar cursos gratis"
+      );
+    }
+  }
+}
+
+class basicStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    if (newCourse.lang !== "english") {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn(
+        "Lo Sentimos, " + this.name + "no puedes tomar cursos en ingles"
+      );
+    }
+  }
+}
+
+class expertStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    this.approvedCourses.push(newCourse);
+  }
+}
+
+const juan = new freeStudent({
   name: "JuanDC",
   username: "JuanDC",
   email: "juanito@platzi.com",
@@ -74,7 +159,7 @@ const juan2 = new Student({
   learningPaths: [escuelaWeb, escuelaVdjgs],
 });
 
-const miguelito2 = new Student({
+const miguelito = new basicStudent({
   name: "miguel",
   username: "miguelito",
   email: "miguel@platzi.com",
